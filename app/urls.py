c070bca6fca3a44ac6029068cf5c5f3025b5384d
GuilhermeSchreiber7 <guilhermeschreiber2007@gmail.com>
2025-04-01 09:29:00 -0300

    feat:  criação da model "editora" e criação da API para Categoria e Editora

--- app/urls.py
+++ app/urls.py
@@ -5,12 +5,16 @@ from drf_spectacular.views import (
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
+
 from rest_framework.routers import DefaultRouter
 
 from core.views import UserViewSet
-
+from core.views import CategoriaViewSet
+from core.views import EditoraViewSet
 router = DefaultRouter()
 
+router.register(r"editoras", EditoraViewSet)
+router.register(r"categorias", CategoriaViewSet)
 router.register(r'usuarios', UserViewSet, basename='usuarios')
 
 urlpatterns = [
---
+++ core/models/editora.py
@@ -0,0 +1,11 @@
+import site
+from django.db import models
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=100)
+    site = models.URLField(max_length=200 , blank=True, null=True)
+    telefone = models.CharField(max_length=15, blank=True, null=True)
+    email = models.EmailField(max_length=100, blank=True, null=True)
+    
+    def __str__(self):
+        return f"({self.id}){self.nome.title()}"
